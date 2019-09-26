f73cbf214b2aae06d1a652b3d9b51131c8a93852
ywu666 <ywu660@aucklanduni.ac.nz>
2019-09-26 17:31:02 +1200

    Removed unnecessary get and set methods for Audio class.

--- src/application/Audio.java
+++ src/application/Audio.java
@@ -4,22 +4,14 @@ import javafx.beans.property.SimpleStringProperty;
 
 public class Audio {
 	
-	private SimpleStringProperty fileName;
+	private Integer fileNum;
 	private SimpleStringProperty mood;
 	private SimpleStringProperty text;
 	
 	public Audio(Integer fileNum, String mood, String text) {
-		this.setFileName(new SimpleStringProperty("audio" + fileNum));
+		this.fileNum = fileNum;
 		this.setMood(new SimpleStringProperty(mood));
 		this.setText(new SimpleStringProperty(text));
-	}
-
-	public String getFileName() {
-		return fileName.get();
-	}
-
-	public void setFileName(SimpleStringProperty fileName) {
-		this.fileName = fileName;
 	}
 
 	public String getMood() {
